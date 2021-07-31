bed9b7d77dd96483555f14cb987c9c16ad5951b7
vandaniel <vandaniel-1994@yndex.ru>
2021-07-31 18:06:33 +0300

    lesson_06

---
+++ src/catalog.php
@@ -0,0 +1,9 @@
+<?php
+	header('content-type: application/json; charset=utf-8');
+	header('Access-Control-Allow-Origin: *');
+
+?>
+[{"title":"Business Suit","price":152,"img":"img/single_page/prod_asl_1.png","icon":"img/single_page/icon/pr1.png"},
+{"title":"aLEXA dress","price":42,"img":"img/single_page/prod_asl_2.png","icon":"img/single_page/icon/pr2.png"},
+{"title":"Sylva trousers","price":52,"img":"img/single_page/prod_asl_3.png","icon":"img/single_page/icon/pr3.png"},
+{"title":"Sylva sweater","price":32,"img":"img/single_page/prod_asl_4.png","icon":"img/single_page/icon/pr4.png"}]
